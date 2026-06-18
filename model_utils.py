babf93224caaacf6dacba6d683288fd6fa300a59
Ygor Reis Gorni <116912324+JonsonVemm@users.noreply.github.com>
2026-06-18 21:27:37 +0000

    Corrige caminho de cache do Hugging Face para o appuser

--- model_utils.py
+++ model_utils.py
@@ -2,12 +2,17 @@ from huggingface_hub import hf_hub_download
 import joblib
 import time
 
-
 def load_model(repo_id: str, filename: str = "model.pkl", force_download: bool = False):
     t0 = time.time()
+    
+    # Adicionamos o cache_dir apontando para dentro do /app
     local_path = hf_hub_download(
-        repo_id=repo_id, filename=filename, force_download=force_download
+        repo_id=repo_id, 
+        filename=filename, 
+        force_download=force_download,
+        cache_dir="/app/model_cache"  # <-- A MÁGICA ACONTECE AQUI
     )
+    
     model = joblib.load(local_path)
     print(f"✅ Modelo carregado do Hub/Cache em {time.time() - t0:.3f}s")
     return model
